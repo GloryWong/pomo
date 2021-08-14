f5b52b588c65661231956c3ba2e6a32e048343a0
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-08-14 15:32:27 +0800

    feat: add pwa

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+  onRegisterError() {},
+})
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'Pomodoro',
+        short_name: 'Pomodoro',
+        description: 'Pomodoro - Zhaozhao',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      // registerType: 'autoUpdate',
+    }),
+  ],
 })
